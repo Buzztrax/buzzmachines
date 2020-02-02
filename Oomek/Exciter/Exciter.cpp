323410abc442cf1e0ca3b424a2d3eb2ede6841d2
Radek Dutkiewicz <oomek@users.noreply.github.com>
2020-02-02 20:30:46 +0000

    Update Exciter.cpp

--- Oomek/Exciter/Exciter.cpp
+++ Oomek/Exciter/Exciter.cpp
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2001 Radoslaw Dutkiewicz <radicdotkey@gmail.com>
+ * Copyright (C) 2003 Radoslaw Dutkiewicz <radicdotkey@gmail.com>
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
